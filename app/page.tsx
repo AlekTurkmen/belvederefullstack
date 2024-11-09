b41e4b84e1d8f3dd42f299511e1f131a553a9520
Alek <alek11221@gmail.com>
2024-11-09 16:31:23 -0500

    meta data title + favicon change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import ProgressDemo from "@/components/ProgressDemo" // Import ProgressDemo componen
 
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[0px_1fr_0px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
