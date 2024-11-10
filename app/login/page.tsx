9be188ca39d23f58d6830026c7dbe4d5e81c4ef7
Alek <alek11221@gmail.com>
2024-11-09 22:27:06 -0500

    login component added (no backend)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,16 +2,12 @@
 //Need to add the login page components to this later
 
 import { login, signup } from './actions'
+import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="flex h-screen w-full items-center justify-center px-4">
+      <LoginForm />
+    </div>
   )
 }
