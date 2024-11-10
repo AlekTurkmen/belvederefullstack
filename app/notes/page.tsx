7bd8e7940d10b9c2959abaf182b229bf4989b7d1
Alek <alek11221@gmail.com>
2024-11-09 21:06:37 -0500

    added error message to data pulling

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -4,7 +4,12 @@ import { createClient } from '@/utils/supabase/server';
 
 export default async function Notes() {
   const supabase = await createClient();
-  const { data: notes } = await supabase.from("UserData").select();
-
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+  const { data: UserData, error } = await supabase.from("UserData").select();
+  
+  if (error) {
+    console.error("Error fetching data:", error);
+    return <pre>Error fetching data</pre>;
+  }
+          
+  return <pre>{JSON.stringify(UserData, null, 2)}</pre>
 }
